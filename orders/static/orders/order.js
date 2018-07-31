e600807e898ad72a22c01ef81afe5afa93d8f03a
Satyanarayana Tammineedi <callsatya801@gmail.com>
2018-07-31 04:04:28 +0000

    javascript toppings working copy

--- orders/static/orders/order.js
+++ orders/static/orders/order.js
@@ -28,15 +28,76 @@ document.addEventListener('DOMContentLoaded', () => {
        }
        toppingAreaChild = document.createElement("div");
        toppingAreaChild.setAttribute("id","ToppingAreaChild");
+       toppingArea.appendChild(toppingAreaChild);
+
        var toppingCount =  button.data('nooftop');
-       var toppings =  button.data('toppings');
-       console.log(`toppingCount : ${toppingCount}`);
-       console.log(`toppings : JSON.parse(${toppings})`);
+       var p_toppings =  button.data('p_toppings');
+       var s_toppings =  button.data('s_toppings');
 
+       var toppings = false;
+      // p_toppings = JSON.parse(JSON.stringify(p_toppings))
+       if(button.data('itemcat') == 'Pizza' && toppingCount > 0)
+       {
+            toppings =p_toppings;
+       }
+       else if (button.data('itemcat') == 'Subs')
+       {
+            toppings = s_toppings;
+       }
+       if(toppings){
+           console.log(`toppingCount : ${toppingCount}`);
+           for(var idx =0; idx < toppings.length; idx++)
+            {
+                console.log(toppings[idx][1]);
+                var seperator = document.createElement('div');
 
+                var checkbox = document.createElement('input');
+                var label = document.createElement('label');
+                checkbox.type = "checkbox";
+                checkbox.name = "topping";
+                checkbox.className = "c_topping";
+                checkbox.value = toppings[idx][1]+'($'+toppings[idx][2] +')';
+                checkbox.id = toppings[idx][0];
+                checkbox.onclick= ()=> {
+                        console.log('Inside validateToppingCount');
+                        console.log(toppingCount);
+                        var checkboxes = document.getElementsByName("topping");
+                        var numberOfCheckedItems = 0;
+                        for(var i = 0; i < checkboxes.length; i++)
+                        {
+                            if(checkboxes[i].checked)
+                                numberOfCheckedItems++;
+                        }
+                        if(numberOfCheckedItems >= toppingCount)
+                        {
+                            console.log('we have reached the limit - of Toppings');
+                            // loop thru and disable all the unchecked
+                            for (var i=0; i< checkboxes.length; i++)
+                             {
+                                 if (! checkboxes[i].checked)
+                                    checkboxes[i].disabled = true;
+                             }
+                        //return false;
+                        }
+                        else
+                        {
+                            //loop thru and enable all
+                            for (var i=0; i< checkboxes.length; i++)
+                             {
+                               checkboxes[i].disabled = false;
+                             }
+                        }
+                };
+
+                label.htmlFor = toppings[idx][0];
+                label.innerText = toppings[idx][1]+'($'+toppings[idx][2] +')';
+                toppingAreaChild.appendChild(seperator);
+                seperator.appendChild(checkbox);
+                seperator.appendChild(label);
+            }
+       }
+      // return false;
     });
 
 });
-
-
 
--- orders/views.py
+++ orders/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 from django.urls import reverse
 from .models import MenuItem, Order, OrderItem
 from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
+
 import json
+
 
 # Create your views here.
 def index(request):
@@ -21,11 +24,19 @@ def index(request):
             itemDict[sc] = items
             menuItems[ic]=itemDict
 
-    # Include Only Toppings
-    Toppings = {}
-    items=  MenuItem.objects.filter(itemSubCategory="Toppings")
-    Toppings=items
+    # Include Only Pizza Toppings
+    pizza_toppings = {}
+    items=  MenuItem.objects.filter(itemCategory="Pizza", itemSubCategory="Toppings").values_list('id','itemName','unitPrice')
+    pizza_toppings= json.dumps(list(items), cls=DjangoJSONEncoder)
+    print(pizza_toppings)
 
-    context = {'menuItems':menuItems, 'toppings':Toppings}
+    # Include Only Subs Toppings
+    subs_toppings = {}
+    items=  MenuItem.objects.filter(itemCategory="Subs", itemSubCategory="Toppings").values_list('id','itemName','unitPrice')
+    subs_toppings= json.dumps(list(items), cls=DjangoJSONEncoder)
+    print(subs_toppings)
+
+
+    context = {'menuItems':menuItems, 'p_toppings':pizza_toppings, 's_toppings':subs_toppings }
     return render(request, "orders/order.html", context)
     #return HttpResponse(menuItems)
